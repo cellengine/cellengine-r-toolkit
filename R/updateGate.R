3043d95297a83385a4d21c35365db40e285cc90f
Zach Bjornson <zbbjornson@gmail.com>
2021-08-10 11:17:11 -0700

    bug(updateGate): should accept byName for gateId, auto_unbox
    Also no query params for this API.

--- R/updateGate.R
+++ R/updateGate.R
@@ -3,19 +3,19 @@
 #' Updates a gate.
 #'
 #' @param experimentId ID of experiment.
-#' @param gateId gate ID
+#' @param gateId ID of gate or a \code{byName} expression.
 #' @param properties Properties to set on the gate.
-#' @param params Optional query parameters.
 #' @export
 #' @examples
 #' \dontrun{
 #' updateGate(experimentId, gateId, list("name" = "new gate name"))
 #' }
 
-updateGate = function(experimentId, gateId, properties = list(), params = list()) {
+updateGate = function(experimentId, gateId, properties = list()) {
   checkDefined(experimentId)
-  checkDefined(gateId)
   experimentId = lookupByName("experiments", experimentId)
-  body = jsonlite::toJSON(properties, null = "null")
-  basePatch(paste("experiments", experimentId, "gates", gateId, sep = "/"), body, params)
+  checkDefined(gateId)
+  gateId = lookupByName(paste("experiments", experimentId, "gates", sep = "/"), gateId)
+  body = jsonlite::toJSON(properties, null = "null", auto_unbox = TRUE)
+  basePatch(paste("experiments", experimentId, "gates", gateId, sep = "/"), body)
 }
