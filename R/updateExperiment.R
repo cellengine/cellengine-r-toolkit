243ba780669c6bf3b0546558f135139ead8daa05
Zach Bjornson <zbbjornson@gmail.com>
2021-08-10 11:39:36 -0700

    bug(updateExperiment): use `auto_unbox=T`

--- R/updateExperiment.R
+++ R/updateExperiment.R
@@ -4,15 +4,14 @@
 #'
 #' @param experimentId ID of experiment.
 #' @param properties Properties to set on the experiment.
-#' @param params Optional query parameters.
 #' @export
 #' @examples
 #' \dontrun{
 #' updateExperiment(experimentId, list("name" = "my experiment"))
 #' }
-updateExperiment = function(experimentId, properties = list(), params = list()) {
+updateExperiment = function(experimentId, properties = list()) {
   checkDefined(experimentId)
   experimentId = lookupByName("experiments", experimentId)
-  body = jsonlite::toJSON(properties, null = "null")
-  basePatch(paste("experiments", experimentId, sep = "/"), body, params)
+  body = jsonlite::toJSON(properties, null = "null", auto_unbox = TRUE)
+  basePatch(paste("experiments", experimentId, sep = "/"), body)
 }
