4fdc0cb76b02c5bf0be3ea7c9d3096806d491730
Zach Bjornson <zbbjornson@gmail.com>
2021-08-09 17:36:46 -0700

    src(scale): change applyScale to take atomic vectors
    Ref https://github.com/primitybio/cellengine-r-toolkit/issues/56#issuecomment-895625680

--- R/applyScale.R
+++ R/applyScale.R
@@ -1,28 +1,32 @@
 #' Apply a scale
 #'
-#' Applies a scale to a list of channel values
+#' Applies a scale to a vector of channel values
 #'
-#' @param scale Scale (named list with keys ['type', 'maximum', 'minimum', 'cofactor']).
-#' @param data List of values for a channel.
+#' @param scale Scale (named list with keys "type", "maximum", "minimum",
+#' and "cofactor").
+#' @param data Vector of values for a channel.
 #' @param clamp_q If true, values will be clamped to fall within the scale's
 #' minimum and maximum.
 #' @export
 #' @examples
 #' \dontrun{
-#' applyScale(list(type='LinearScale', minimum=1, maximum=10), list(1, 2, 3, 4, 5))
+#' applyScale(list(type='LinearScale', minimum=1, maximum=10), c(1, 2, 3, 4, 5))
+#'
+#' # Using a Scale from a CellEngine ScaleSet
+#' scaleSet = getScaleSets(experimentId)
+#' chanIdx = 5
+#' applyScale(scaleSet$scales[[1]][chanIdx, "scale"], c(1, 2, 3, 4, 5))
 #' }
 applyScale = function(scale, data, clamp_q=FALSE) {
-
   fn = switch(
     scale$type,
     "LinearScale" = function(a) { a },
-    "LogScale" = function(a) { if (a <= 1) 0 else log10(a) },
+    "LogScale" = function(a) { log10(pmax(1, a)) },
     "ArcSinhScale" = function(a) { asinh(a / scale$cofactor) },
     )
 
-  if (clamp_q) {
-    lapply(pmax(pmin(data, scale$maximum), scale$minimum), fn)
-  } else {
-    lapply(data, fn)
-  }
+  if (clamp_q)
+    data = pmax(pmin(data, scale$maximum), scale$minimum)
+
+  fn(data)
 }
