b0e56e05a787578b58cdd1dd7ba32671edff054a
Zach Bjornson <zbbjornson@gmail.com>
2021-08-10 11:10:38 -0700

    feat(pop): add updatePopulation function
    Fixes #66

---
+++ R/updatePopulation.R
@@ -0,0 +1,23 @@
+#' Update population
+#'
+#' Updates a population.
+#'
+#' @param experimentId ID of experiment.
+#' @param populationId Population ID or a \code{byName()} expression.
+#' @param properties Properties to set on the population.
+#' @export
+#' @examples
+#' \dontrun{
+#' updatePopulation(experimentId, populationId, list("name" = "new pop name"))
+#' }
+
+updatePopulation = function(experimentId,
+                            populationId,
+                            properties = list()) {
+  checkDefined(experimentId)
+  experimentId = lookupByName("experiments", experimentId)
+  checkDefined(populationId)
+  populationId = lookupByName(paste("experiments", experimentId, "populations", sep = "/"), populationId)
+  body = jsonlite::toJSON(properties, null = "null", auto_unbox = TRUE)
+  basePatch(paste("experiments", experimentId, "populations", populationId, sep = "/"), body)
+}
---
+++ tests/testthat/test-updatePopulation.R
@@ -0,0 +1,27 @@
+context("updatePopulation")
+
+test_that("Correct HTTP request is made", {
+  with_mock(
+    `httr::request_perform` = function(req, handle, refresh) {
+      expect_equal(req$method, "PATCH")
+      expect_equal(req$url, "https://my.server.com/api/v1/experiments/591a3b441d725115208a6fda/populations/592640aa298f1480900e10e4")
+      body = rawToChar(req$options$postfields)
+      expect_equal(body, '{"name":"new name"}')
+
+      response = httptest::fake_response(
+        req$url,
+        req$method,
+        content='{"experimentId":"591a3b441d725115208a6fda","name":"new name","parentId":null,"gates":"{\\\"$and\\\":[\\\"591a3b441d725115203a6fda\\\"]}","terminalGateGid":"591a3b441d725115203a6fda"}',
+
+        status_code = 200,
+        headers = list(`Content-Type` = "application/json")
+      )
+      return(response)
+    },
+    {
+      setServer("https://my.server.com")
+      resp = updatePopulation("591a3b441d725115208a6fda", "592640aa298f1480900e10e4", list("name" = "new name"))
+      expect_equal(resp$name, "new name")
+    }
+  )
+})
