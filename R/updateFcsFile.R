9830f78b78f200914503f25170cb1c4810bd6196
gegnew <g.egnew@gmail.com>
2022-04-12 12:48:31 +0200

    feat(fcsfile): add updateFcsFile

---
+++ R/updateFcsFile.R
@@ -0,0 +1,36 @@
+#' Update Fcs File
+#'
+#' Updates an fcs file
+#'
+#' @param experimentId character ID of experiment.
+#' @param fcsFileId character ID of file or a \code{byName} expression.
+#' @param properties list Properties to set on the fcs file.
+#'
+#' Accepted properties:
+#' - filename character The name of the file
+#' - panel list List with properties:
+#'   - channel character
+#'   - index character
+#'   - reagent character Optional; only channel and index are required.
+#' - panelName character
+#' - isControl logical
+#' - compensation character Compensation ID
+#' - deleted string ISO 8601 date string, "%Y-%m-%dT%H:%M:%S.%fZ"
+#' - gatesLocked logical
+#' - annotations list List with properties:
+#'   - name character
+#'   - value character
+#' @md
+#' @export
+#' @examples
+#' \dontrun{
+#' updateFcsFile(experimentId, list("filename" = "my fcs file"))
+#' }
+updateFcsFile <- function(experimentId, fcsFileId, properties = list()) {
+  checkDefined(experimentId)
+  experimentId <- lookupByName("experiments", experimentId)
+  checkDefined(fcsFileId)
+  fcsFileId <- lookupByName(paste("experiments", experimentId, "fcsfiles", sep = "/"), fcsFileId, "filename")
+  body <- jsonlite::toJSON(properties, null = "null", auto_unbox = TRUE)
+  basePatch(paste("experiments", experimentId, "fcsfiles", fcsFileId, sep = "/"), body)
+}
