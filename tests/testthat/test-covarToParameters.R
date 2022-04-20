9fb8f45c6ac760af7a354e1648228ef1be8fa1a1
gegnew <g.egnew@gmail.com>
2022-04-20 16:21:38 +0200

    src: minor formatting fixes

--- tests/testthat/test-covarToParameters.R
+++ tests/testthat/test-covarToParameters.R
@@ -1,9 +1,13 @@
 context("covarToParameters")
 
 test_that("returns ellipse parameters from covariance matrix", {
-  covar <- matrix(c(6879, 3612, 3612, 5215), nrow = 2, ncol = 2, byrow = TRUE,
-         dimnames = list(c("FSC-A", "FSC-W"),
-                         c("FSC-A", "FSC-W")))
+  covar <- matrix(c(6879, 3612, 3612, 5215),
+    nrow = 2, ncol = 2, byrow = TRUE,
+    dimnames = list(
+      c("FSC-A", "FSC-W"),
+      c("FSC-A", "FSC-W")
+    )
+  )
   params <- covarToParameters(covar)
 
   expect_equal(params$major, 98.8, tolerance = 0.001)
@@ -13,9 +17,13 @@ test_that("returns ellipse parameters from covariance matrix", {
 
 test_that("returns parameters for test matrix", {
   # https://cookierobotics.com/007/
-  covar <- matrix(c(9, 5, 5, 4), nrow = 2, ncol = 2, byrow = TRUE,
-         dimnames = list(c("FSC-A", "FSC-W"),
-                         c("FSC-A", "FSC-W")))
+  covar <- matrix(c(9, 5, 5, 4),
+    nrow = 2, ncol = 2, byrow = TRUE,
+    dimnames = list(
+      c("FSC-A", "FSC-W"),
+      c("FSC-A", "FSC-W")
+    )
+  )
   params <- covarToParameters(covar)
 
   expect_equal(params$major, 3.477, tolerance = 0.001)
@@ -24,9 +32,13 @@ test_that("returns parameters for test matrix", {
 })
 
 test_that("returns ellipse parameters for a unit circle", {
-  covar <- matrix(c(1, 0, 0, 1), nrow = 2, ncol = 2, byrow = TRUE,
-         dimnames = list(c("FSC-A", "FSC-W"),
-                         c("FSC-A", "FSC-W")))
+  covar <- matrix(c(1, 0, 0, 1),
+    nrow = 2, ncol = 2, byrow = TRUE,
+    dimnames = list(
+      c("FSC-A", "FSC-W"),
+      c("FSC-A", "FSC-W")
+    )
+  )
   params <- covarToParameters(covar)
 
   expect_equal(params$major, 1, tolerance = 0.001)
